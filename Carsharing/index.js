5738717477e4166c60ad5fdf1088ac64e5be36ae
frankela <lara.sofie.franke@hs-furtwangen.de>
2023-06-08 21:38:42 +0200

    trying server

--- Carsharing/index.js
+++ Carsharing/index.js
@@ -1,6 +1,6 @@
 import { Car } from "./class/car.js";
 import { HelpFunktions } from "./class/function.js";
-localStorage.setItem("lastmove", "index.html");
+//localStorage.setItem("lastmove","index.html");
 HelpFunktions.navibar();
 // gets Page content
 Car.showCarData();
--- Carsharing/index.ts
+++ Carsharing/index.ts
@@ -2,7 +2,7 @@ import {Car} from "./class/car.js";
 import{HelpFunktions} from "./class/function.js"
 
 
-localStorage.setItem("lastmove","index.html");
+//localStorage.setItem("lastmove","index.html");
 HelpFunktions.navibar();
 // gets Page content
 Car.showCarData();
